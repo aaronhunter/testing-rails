7fe1290db3ad14ca93c540e3327e0e9ba54eb006
Aaron Hunter <aaron@voxable.io>
2017-03-13 13:28:59 -0500

    Add homepage spec

--- spec/features/user_views_homepage_spec.rb
+++ spec/features/user_views_homepage_spec.rb
@@ -1,5 +1,8 @@
+require 'rails_helper'
+
 RSpec.feature "User visits the homepage" do
   scenario  "they see existing links" do
-
+    visit root_path
+    expect(page).to have_link link.title, href: link.url
   end
 end
