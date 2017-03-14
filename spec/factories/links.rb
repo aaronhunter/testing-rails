4fc18383196d93cd5d80e1a2f4027e313106321b
Aaron Hunter <aaron@voxable.io>
2017-03-14 09:59:32 -0500

    Remove auto factory

--- spec/factories/links.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :link do
-    title "MyString"
-    url "MyString"
-  end
-end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,5 +1,5 @@
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods”
+  config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
     begin
