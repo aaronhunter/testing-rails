45b0249dcd87a32bdc9e06e7c17bfc4ee9dc195c
Aaron Hunter <aaron@voxable.io>
2017-05-12 15:57:06 -0500

    Add controller documentation

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,13 +1,16 @@
 # Controller for Link objects
 class LinksController < ApplicationController
+  # Show all links
   def index
     @links = Link.all
   end
 
+  # New link
   def new
     @link = Link.new
   end
 
+  # Crud
   def create
     @link = Link.new(link_params)
 
@@ -18,6 +21,7 @@ class LinksController < ApplicationController
     end
   end
 
+  # cRud
   def show
     @link = Link.find(params[:id])
   end
