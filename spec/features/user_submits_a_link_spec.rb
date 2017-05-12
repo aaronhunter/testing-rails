0e9802f9b1ccdff5462edc433bf9dff2bc7e3b42
Aaron Hunter <aaron@voxable.io>
2017-05-12 15:57:38 -0500

    Add invalid form specs for link and title for practice

--- spec/features/user_submits_a_link_spec.rb
+++ spec/features/user_submits_a_link_spec.rb
@@ -1,29 +1,43 @@
 require 'rails_helper'
 
-RSpec.feature "User submits a link" do
-  scenario "they see the page for the submitted link" do
-    link_title = "This Testing Rails book is awesome!"
-    link_url = "http://testingrailsbook.com"
+RSpec.feature 'User submits a link' do
+  scenario 'they see the page for the submitted link' do
+    link = create(:link)
 
     visit root_path
-    click_on  "Submit a new link"
-    fill_in "link_title", with: link_title
-    fill_in "link_url", with: link_url
-    click_on "Submit!"
+    click_on  'Submit a new link'
+    fill_in 'link_title', with: link.title
+    fill_in 'link_url', with: link.url
+    click_on 'Submit!'
 
-    expect(page).to have_link link_title, href: link_url
+    expect(page).to have_link link.title, href: link.url
   end
 
-  context "the form is invalid" do
-    scenario "they see an error message" do
-      link_title = "This Testing Rails book is awesome!"
+  context 'the form is invalid' do
+    context 'no url is present' do
+      scenario 'they see an error message' do
+        link_title = 'This Testing Rails book is awesome!'
 
-      visit root_path
-      click_on "Submit a new link"
-      fill_in "link_title", with: link_title
-      click_on "Submit!"
+        visit root_path
+        click_on 'Submit a new link'
+        fill_in 'link_title', with: link_title
+        click_on 'Submit!'
 
-      expect(page).to have_content "Url can't be blank"
+        expect(page).to have_content "Url can't be blank"
+      end
+    end
+
+    context 'no title is present' do
+      scenario 'they see an error message' do
+        link_url = 'http://testingrailsbook.com'
+
+        visit root_path
+        click_on 'Submit a new link'
+        fill_in 'link_url', with: link_url
+        click_on 'Submit!'
+
+        expect(page).to have_content "Title can't be blank"
+      end
     end
   end
 end
