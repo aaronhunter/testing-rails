30818fd9b30e7675b9c4481fe18defca03b3df44
Aaron Hunter <aaron@voxable.io>
2017-08-14 11:48:45 -0500

    Bundle, use let

--- spec/features/user_views_homepage_spec.rb
+++ spec/features/user_views_homepage_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'User visits the homepage' do
   scenario 'they see existing links' do
-    link = create(:link)
+    let(:link) { create(:link) }
 
     visit root_path
     expect(page).to have_link link.title, href: link.url
