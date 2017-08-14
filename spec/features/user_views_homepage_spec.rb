44658702a746729a48ea41ea30290972aceb8946
Aaron Hunter <aaron@voxable.io>
2017-08-14 12:48:52 -0500

    Favor scenario syntax in this case

--- spec/features/user_views_homepage_spec.rb
+++ spec/features/user_views_homepage_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.feature 'User visits the homepage' do
-  scenario 'they see existing links' do
-    let(:link) { create(:link) }
-
+  scenario 'should show existing links' do
+    link = create(:link)
     visit root_path
     expect(page).to have_link link.title, href: link.url
   end
